f1b56f0ed05af69f3b34dfc967c675d28f4806f9
neskair-ia <neskair.ia@gmail.com>
2026-08-10 23:41:17 -0300

    deploy: actualizaciÃ³n automÃ¡tica 2026-08-10 23:41:17

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://jbingenieriacorporativa.com',
   output: 'static',
+  trailingSlash: 'always',
   vite: {
     plugins: [tailwindcss()]
   },
